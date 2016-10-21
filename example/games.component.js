ab4fbc92c25fef1201c89d3937ec7eba95f3431b
zhipeng <zhangzhipeng@973.sinanet.com>
2016-10-21 19:40:52 +0800

    调整结构，自定义controller更简洁

--- example/games.component.js
+++ example/games.component.js
@@ -10,5 +10,25 @@ module.exports = {
         content       : { type: String, required: true, trim: true, joi: Joi.string() },
         cover         : { type: String, required: true, trim: true, joi: Joi.string().uri({scheme:/https?/}) },
         media         : { type: String, required: true, trim: true, joi: Joi.string().uri({scheme:/https?/}) }
+    },
+    Options: {
+        controllers: {
+            getAll: {
+                filter: {
+                    _id: false,
+                    id: true
+                },
+                condition: {
+                    deleted: {$ne: true},
+                    title: {$ne: 'abc'}
+                }
+            },
+            getOne: {
+                filter: {
+                    _id: true,
+                    id: false
+                }
+            }
+        }
     }
 }
